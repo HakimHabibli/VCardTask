03bfb9347ceff2f45224e4c23f95a9e551d57998
HakimHabibli <HakimHabibli>
2024-08-25 13:39:51 +0400

    firstcommit

---
+++ QRCode/Controllers/QRCodeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using static QRCoder.PayloadGenerator;
+
+namespace QRCode.Controllers
+{
+    public class QRCodeController : Controller
+    {
+        public IActionResult Index()
+        {
+            QRCode.Models.QRCodeModel model = new();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(QRCode.Models.QRCodeModel model)
+        {
+            Payload? payload = null;
+
+            switch (model.QRCodeType)
+            {
+                case 1: // compose sms
+                    payload = new SMS(model.SMSPhoneNumber, model.SMSBody);
+                    break;
+                case 2: //compose email
+                    payload = new Mail(model.ReceiverEmailAddress, model.EmailSubject, model.EmailMessage);
+                    break;
+            }
+
+            QRCodeGenerator qrGenerator = new();//QRCodeGenerator klassina aid bir obyekt yaradiriq
+
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(payload);//həmin yaratdığımız obyekti məlumatlarla birlikdə QRCodeGenerator klassinin methodundan istifade edib melumatlarila birlikdə yaradiriq 
+
+
+
+            BitmapByteQRCode qrCode = new BitmapByteQRCode(qrCodeData);
+            string base64String = Convert.ToBase64String(qrCode.GetGraphic(20));
+            model.QRImageURL = "data:image/png;base64," + base64String;
+
+            return View(model);
+
+        }
+    }
+
+
+}
---
+++ QRCode/Models/QRCodeModel.cs
@@ -0,0 +1,17 @@
+﻿namespace QRCode.Models;
+
+public class QRCodeModel
+{
+    public int QRCodeType { get; set; }
+    public string QRImageURL { get; set; } = string.Empty;
+
+    // email 
+    public string ReceiverEmailAddress { get; set; } = string.Empty;
+    public string EmailSubject { get; set; } = string.Empty;
+    public string EmailMessage { get; set; } = string.Empty;
+
+    // sms 
+    public string SMSPhoneNumber { get; set; } = string.Empty;
+    public string SMSBody { get; set; } = string.Empty;
+
+}
